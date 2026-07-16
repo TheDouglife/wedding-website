7c50544c988cf6533dc4dd1b530cd9a8ac9038a1
Doug Montgomery <doug@douglife.com>
2026-07-16 19:56:39 -0400

    vite confoig change

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'dl-wedding-app-598yl.ondigitalocean.app',
+      'rowsalie.com',
+      'www.rowsalie.com'
+    ]
+  }
 });
